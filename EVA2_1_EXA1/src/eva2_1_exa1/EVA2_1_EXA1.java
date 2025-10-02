9bc6cd7f812a766f353d33b74f2ee3b9bb9d2928
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-10-01 23:10:53 -0600

    Add files via upload

---
+++ EVA2_1_EXA1/src/eva2_1_exa1/EVA2_1_EXA1.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1_exa1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author igrid
+ */
+public class EVA2_1_EXA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        Scanner captu = new Scanner(System.in);
+        int saldo = 5000;
+        int calculo;
+        int pin, opc, monto;
+        System.out.println("Introduce el PIN: ");
+        pin = captu.nextInt();
+        if(pin == 1234) {
+            System.out.println("1. Consultar saldo...");
+            System.out.println("2. Retirar monto...");
+            System.out.println("3. Depositar monto...");
+            System.out.println("Selecciona la operación.");
+            opc = captu.nextInt();
+            if(opc == 1) {
+                System.out.println("Tu saldo es de " + saldo);
+            }else if (opc == 2){
+                System.out.println("Cuanto vas a retirar?");
+                monto = captu.nextInt();
+                if (monto <= saldo){
+                    calculo = saldo - monto;
+                    System.out.println("Tu saldo es de " + calculo);
+                }else{
+                    System.out.println("Monto mayor al saldo!");
+                }
+            }else if (opc == 3) {
+                System.out.println("Cuanto vas a depositar?");
+                monto = captu.nextInt();
+                if(monto >= 0) {
+                    calculo = saldo + monto;
+                System.out.println("Tu saldo es de" + calculo);
+                }else{
+                    System.out.println("Monto Negativo!");
+                }
+            }else{
+                System.out.println("Opción incorrecta!");
+            }
+        }else{
+            System.out.println("PIN incorrecto!");
+                }
+            }
+        }
+        
+    
+
