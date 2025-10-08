988315418880cfc4321aa6b769597158f05614c5
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-10-08 16:51:03 -0600

    Add files via upload

---
+++ EVA2_14_CAPTURA/src/eva2_14_captura/EVA2_14_CAPTURA.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_14_captura;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Antonio
+ */
+public class EVA2_14_CAPTURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner captu = new Scanner(System.in);
+        int tama;
+        System.out.println("Cuantas calificaciones va a capturar?");
+        tama = captu.nextInt();
+        int califas[] = new int[tama];
+        //CAPTURAR
+        for(int i = 0; i < califas.length; i++) {
+        System.out.println("Calificacion " + (i + 1) + ":");
+        califas[i] = captu.nextInt();
+    }
+        //LEER
+     for(int i = 0; i < califas.length; i++){
+        System.out.println("[" + califas[i] + "]");
+     }
+}
+}
