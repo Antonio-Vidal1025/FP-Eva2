ec081f98dc1710c704b5810abdff2bba98cb7b6c
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-10-27 16:48:30 -0600

    Add files via upload

---
+++ EVA2_17_ALCANCE/src/eva2_17_alcance/EVA2_17_ALCANCE.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17_alcance;
+
+/**
+ *
+ * @author igrid
+ */
+public class EVA2_17_ALCANCE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 5;// este i existe desde la primera hasta la ultima llave
+        {
+            System.out.println(i);
+        int valor = 100;
+        System.out.println(valor);
+        
+    }
+        System.out.println(valor);
+    }
+    
+}
