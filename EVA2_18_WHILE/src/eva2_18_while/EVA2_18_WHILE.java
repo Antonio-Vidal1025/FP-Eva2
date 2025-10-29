a58abb5e0b9ccbadbd2245b5a29ec0e6ce989fb3
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-10-28 23:55:56 -0600

    Add files via upload

---
+++ EVA2_18_WHILE/src/eva2_18_while/EVA2_18_WHILE.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_while;
+
+/**
+ *
+ * @author Antonio
+ */
+public class EVA2_18_WHILE {
+
+
+    public static void main(String[] args) {
+        //SIMULAR UN FOR CON WHILE
+        System.out.println("CICLO WHILE------");
+    int i = 1;
+    while(i<=10) {//se repite si la codicion es verdad
+        System.out.println("Antonio Vidal");
+        i++;
+    }
+    //SIMULAR UN FOR CON DO WHILE
+        System.out.println("CICLO DO WHILE------");
+       i = 1;
+    do{
+        System.out.println("Antonio Vidal");
+        i++;
+    } while (i<=10);
+    }
+}
