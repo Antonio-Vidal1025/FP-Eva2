a24d0c116baa04bcdeefffd17311424dd534316f
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-11-06 16:08:28 -0600

    Add files via upload

---
+++ EVA2_20_CICLO_INFINITO/src/eva2_20_ciclo_infinito/EVA2_20_CICLO_INFINITO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_20_ciclo_infinito;
+
+/**
+ *
+ * @author Antonio
+ */
+public class EVA2_20_CICLO_INFINITO {
+
+    public static void main(String[] args) {
+while (true){
+    System.out.println("Antonio Vidal");
+}
+    }
+    
+}
