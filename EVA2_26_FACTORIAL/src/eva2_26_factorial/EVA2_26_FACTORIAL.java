ae019e69d4bcfcb3c29bd97884aaa491346d469f
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-11-06 16:11:09 -0600

    Add files via upload

---
+++ EVA2_26_FACTORIAL/src/eva2_26_factorial/EVA2_26_FACTORIAL.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_26_factorial;
+
+/**
+ *
+ * @author Antonio
+ */
+public class EVA2_26_FACTORIAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here}
+        Scanner captu = new Scanner (System.in);
+        int val, facotrial = 1;
+        
+    }
+    
+}
